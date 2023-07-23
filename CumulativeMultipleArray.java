1d5c284424c760e07cc4185fe401175183c0bd09
Bhavana7523 <130962685+Bhavana7523@users.noreply.github.com>
2023-07-23 18:39:29 +0530

    Update CumulativeMultipleArray.java

--- CumulativeMultipleArray.java
+++ CumulativeMultipleArray.java
@@ -9,32 +9,32 @@ Sample input: arrNum = [5, 3, 4, 2, 0, 8]
 
 import java.util.Scanner;
 
-public class CumulativeMultipleArray {
+public class CMA{
     public static void main(String[] args) {
         try {
             Scanner scanner = new Scanner(System.in);
 
             // Task 1: Read input array
             System.out.print("Enter the number of elements in the array: ");
-            int n = scanner.nextInt();
-            int[] array = new int[n];
+            int length = scanner.nextInt();
+            int[] array18 = new int[length];
 
             System.out.println("Enter " + n + " integers:");
-            for (int i = 0; i < n; i++) {
-                array[i] = scanner.nextInt();
+            for (int i = 0; i < length; i++) {
+                array18[i] = scanner.nextInt();
             }
 
             // Task 2: Calculate cumulative multiple and push into the same array
-            int cumulativeMultiple = 1;
-            for (int i = 0; i < n; i++) {
-                cumulativeMultiple *= array[i];
-                array[i] = cumulativeMultiple;
+            int multiple = 1;
+            for (int i = 0; i < length; i++) {
+                multiple*= array[18i];
+                array[i] = multiple;
             }
 
             // Task 3: Print the resulting array
             System.out.println("\nResulting array with cumulative multiples:");
-            for (int i = 0; i < n; i++) {
-                System.out.print(array[i] + " ");
+            for (int i = 0; i < length; i++) {
+                System.out.print(array18[i] + " ");
             }
         } catch (Exception e) {
             System.out.println("Error occurred: " + e.getMessage());
