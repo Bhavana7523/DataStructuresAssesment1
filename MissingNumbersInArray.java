21dcd9739c2d5c9090532d950e5e43a725348631
Bhavana7523 <130962685+Bhavana7523@users.noreply.github.com>
2023-07-23 20:08:08 +0530

    Update MissingNumbersInArray.java

--- MissingNumbersInArray.java
+++ MissingNumbersInArray.java
@@ -9,43 +9,48 @@ Sample input: arrNum = {1, 2, 3, 5, 6, 8, 10, 11, 14}
 Output: 4, 7, 9, 12, 13
 
 
+import java.util.*;
+	public class MNr {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner sc=new Scanner(System.in);
+			int length=sc.nextInt();
+			int arrNum1[]=new int[length];
+			for(int i=0;i<length;i++) {
+				arrNum1[i]=sc.nextInt();
+				
+			}
+			int num1=arrNum1[0];
+			for(int i=1;i<length;i++) {
+				num1=num1+1;
+				if(arrNum1[i]==num1) {
+					continue;
+				}
+				else {
+					int difference=arrNum1[i]-num1+1;
+					if(difference>1) {
+						while(difference>1) {
+							System.out.print(num1+" ");
+							num1=num1+1;
+							difference--;
+						}
+						
+					}
+					else {
+						System.out.print(num1+" ");
+						num1=arrNum1[i];
+					}
+								
+				}
+				
+			}
+			
+
+		}
+
+	}
 
 
-
-
-import java.util.Scanner;
-
-public class MissingNumbersInArray {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the number of elements in the array: ");
-        int length = scanner.nextInt();
-        int[] Number1 = new int[n];
-
-        System.out.println("Enter " + length + " integers:");
-        for (int i = 0; i < length; i++) {
-            Number1[i] = scanner.nextInt(length);
-        }
-
-        System.out.println("Missing numbers in the array:");
-            PMN(Number1);
-    }
-
-    public static void PMN(int[] arr18) {
-        int eN = arr18[0];
-        int i= 0;
-
-        while (i < arr18.length) {
-            if (arr[i] != eN) {
-                System.out.print(eN);
-                if (i <arr18.length-1) {
-                    System.out.print(" , ");
-                }
-            } else {
-                i++;
-            }
-            eN++;
-        }
-    }
-}
+			
+					
